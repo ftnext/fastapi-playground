af482bb3e60e1900316d2858c2a129482f05b1f9
ftnext <takuyafjp+develop@gmail.com>
2025-02-12 22:59:14 +0900

    [feat] Postgres sync connection example

---
+++ database/connection/postgres_sync.py
@@ -0,0 +1,61 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "sqlmodel",
+#     "SQLAlchemy[postgresql]",
+# ]
+# ///
+# Based on: https://gist.github.com/ftnext/a15376f38433ffeb5c613d5e76b4ee61
+import logging
+from pathlib import Path
+
+from sqlmodel import Field, Session, SQLModel, create_engine
+
+logging.basicConfig(
+    filename=Path(__file__).with_suffix(".log"),
+    format="%(asctime)s | %(levelname)s | %(name)s:%(funcName)s:%(lineno)d - %(message)s",
+)
+logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str
+    age: int | None = None
+
+
+"""Prerequisites:
+
+docker run --rm --name postgres \
+    -e POSTGRES_USER=developer \
+    -e POSTGRES_PASSWORD=mysecretpassword \
+    -e POSTGRES_DB=practice \
+    -p 5432:5432 -d postgres:15.10
+"""
+database_url = "postgresql://developer:mysecretpassword@127.0.0.1:5432/practice"
+engine = create_engine(database_url)
+
+if __name__ == "__main__":
+    SQLModel.metadata.create_all(engine)
+
+    hero_1 = Hero(name="Deadpond", secret_name="Dive Wilson")
+    print("Before insert:", hero_1)
+    # Before insert: name='Deadpond' secret_name='Dive Wilson' id=None age=None
+
+    with Session(engine) as session:
+        session.add(hero_1)
+        print("After add:", hero_1)
+        # After add: name='Deadpond' secret_name='Dive Wilson' id=None age=None
+
+        session.commit()
+        print("After commit:", hero_1)
+        # After commit:
+
+        print("After commit (access id):", hero_1.id)
+        # After commit (access id): 1
+        print("After id access:", hero_1)
+        # After id access: id=1 secret_name='Dive Wilson' name='Deadpond' age=None
+
+    print("After session close:", hero_1)
+    # After session close: secret_name='Dive Wilson' name='Deadpond' id=1 age=None
