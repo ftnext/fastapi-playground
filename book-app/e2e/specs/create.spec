76339cff79674bcb23794620a31fef483a6fb251
ftnext <takuyafjp+develop@gmail.com>
2025-02-15 16:37:07 +0900

    [test] POST /books

---
+++ book-app/e2e/specs/create.spec
@@ -0,0 +1,10 @@
+# Bookを登録できる
+
+## /booksにPOSTリクエストを送って、Bookを登録できる
+* パス"/books"に
+* メソッド"POST"で
+* メディアタイプ"application/json"で
+* JSON<file:fixtures/books/post/request.json>で
+* リクエストを送る
+* レスポンスのステータスコードが
+* 整数値の"201"である
--- book-app/e2e/step_impl/playtest2.py
+++ book-app/e2e/step_impl/playtest2.py
@@ -1,3 +1,4 @@
+import json
 import os
 from urllib.parse import urljoin
 
@@ -17,11 +18,22 @@ def set_method(method: str):
     data_store.spec["method"] = method
 
 
+@step("メディアタイプ<media_type>で")
+def set_content_type_header(media_type: str):
+    data_store.spec.setdefault("kwargs", {})["headers"] = {"Content-Type": media_type}
+
+
+@step("JSON<json_data>で")
+def set_json_data(json_str: str):
+    data_store.spec.setdefault("kwargs", {})["json"] = json.loads(json_str)
+
+
 @step("リクエストを送る")
 def send_request():
     endpoint = urljoin(BASE_URL, data_store.spec["path"])
     method = data_store.spec["method"].lower()
-    response = httpx.request(method, endpoint)
+    kwargs = data_store.spec.get("kwargs", {})
+    response = httpx.request(method, endpoint, **kwargs)
     data_store.spec["response"] = response
 
 
