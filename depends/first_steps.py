f257a9a75a776bf72099bfb622303c797d8c40a2
ftnext <takuyafjp+develop@gmail.com>
2025-02-20 22:44:13 +0900

    [feat] See Depends' function is called per request

---
+++ depends/first_steps.py
@@ -0,0 +1,24 @@
+# https://fastapi.tiangolo.com/tutorial/dependencies/#share-annotated-dependencies
+from typing import Annotated
+
+from fastapi import Depends, FastAPI
+
+app = FastAPI()
+
+
+async def common_parameters(q: str | None = None, skip: int = 0, limit: int = 100):
+    print("common_parameters called")
+    return {"q": q, "skip": skip, "limit": limit}
+
+
+CommonsDep = Annotated[dict, Depends(common_parameters)]
+
+
+@app.get("/items/")
+async def read_items(commons: CommonsDep):
+    return commons
+
+
+@app.get("/users/")
+async def read_users(commons: CommonsDep):
+    return commons
