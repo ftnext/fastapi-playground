3a621a8cb0033fbc180606ceb9384fb94fec3791
ftnext <takuyafjp+develop@gmail.com>
2025-02-21 19:22:28 +0900

    [refactor] Inject use case

--- book-app/api/src/books_api/rest/app.py
+++ book-app/api/src/books_api/rest/app.py
@@ -1,9 +1,10 @@
+from typing import Annotated
+
 from fastapi import Depends, FastAPI, status
 from sqlmodel.ext.asyncio.session import AsyncSession as SQLModelAsyncSession
 
 from books_api.driver.database import PostgresqlDatabaseDriver, get_session
 from books_api.gateway.fetch_books import FetchBooksFromDatabase
-from books_api.port.fetch_books import FetchBooksPort
 from books_api.rest.presentations import BookReadModel
 from books_api.use_case.list_books import ListBooksUseCase
 
@@ -15,14 +16,15 @@ def hello():
     return {"message": "Hello World"}
 
 
-def get_books_deps(
-    session: type[SQLModelAsyncSession] = Depends(get_session),
-) -> FetchBooksPort:
-    return FetchBooksFromDatabase(PostgresqlDatabaseDriver(session))
+def inject_list_books_use_case(
+    session: Annotated[type[SQLModelAsyncSession], Depends(get_session)],
+) -> ListBooksUseCase:
+    return ListBooksUseCase(FetchBooksFromDatabase(PostgresqlDatabaseDriver(session)))
 
 
 @app.get("/books", response_model=list[BookReadModel], status_code=status.HTTP_200_OK)
-async def get_books(fetch_books_port: FetchBooksPort = Depends(get_books_deps)):
-    use_case = ListBooksUseCase(fetch_books_port)
+async def get_books(
+    use_case: Annotated[ListBooksUseCase, Depends(inject_list_books_use_case)],
+):
     books = await use_case.execute()
     return [BookReadModel.from_book(book) for book in books]
