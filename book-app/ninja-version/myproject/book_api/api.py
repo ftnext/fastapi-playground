a22a54f1b40a2212fc381308e53a211d4ae9d80a
ftnext <takuyafjp+develop@gmail.com>
2025-06-29 17:20:48 +0900

    [docs] sync_to_async alternative

--- book-app/ninja-version/myproject/book_api/api.py
+++ book-app/ninja-version/myproject/book_api/api.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 
+from asgiref.sync import sync_to_async
 from ninja import NinjaAPI, Schema
 
 from .models import Book
@@ -27,11 +28,13 @@ class BookOut(Schema):
 
 @api.get("/books", response=list[BookOut])
 async def get_books(request):
+    # all_books = await sync_to_async(list)(Book.objects.all())
     all_books = [book async for book in Book.objects.all()]
     return all_books
 
 
 @api.post("/books", response={201: BookOut})
 async def create_book(request, payload: BookIn):
+    # book = await sync_to_async(Book.objects.create)(**payload.dict())
     book = await Book.objects.acreate(**payload.dict())
     return book
