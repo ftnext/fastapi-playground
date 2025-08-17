e81dded8db4178d679bdf4693437096f86aa09cd
ftnext <takuyafjp+develop@gmail.com>
2025-08-17 21:28:58 +0900

    [feat] Get GitHub discussion count

---
+++ tiangolo-2025-talk/call_github.py
@@ -0,0 +1,34 @@
+import os
+
+import httpx
+
+graphql_query = """
+query Q($name: String!, $owner: String!, $answered: Boolean) {
+  repository(name: $name, owner: $owner) {
+    discussions(answered: $answered) {
+      totalCount
+    }
+  }
+}
+"""
+
+
+async def get_github_discussion_count(owner: str, name: str, answered: bool):
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            "https://api.github.com/graphql",
+            json={
+                "query": graphql_query,
+                "variables": {"name": name, "owner": owner, "answered": answered},
+            },
+            headers={"Authorization": f"Bearer {os.getenv('GITHUB_TOKEN')}"},
+        )
+        response.raise_for_status()
+        return response.json()
+
+
+if __name__ == "__main__":
+    import asyncio
+
+    data = asyncio.run(get_github_discussion_count("fastapi", "fastapi", True))
+    print(data)
