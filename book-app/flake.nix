ca171d12d0e0581147618ff1489e245b99306618
ftnext <takuyafjp+develop@gmail.com>
2026-03-29 15:38:24 +0900

    feat: nix develop hello world
    Thanks to https://zenn.dev/comamoca/articles/2025-02-09-start-to-use-nix-flake-by-one-file

---
+++ book-app/flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      ...
+    }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = [
+          pkgs.hello
+        ];
+      };
+      packages.${system}.default = pkgs.hello;
+    };
+}
